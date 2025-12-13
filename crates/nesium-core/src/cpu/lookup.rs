17dfcb549c105406cc0accbb36e0729fa02d6d87
mikai <dreamfever2017@yahoo.com>
2025-12-13 19:01:34 +0800

    style(cpu/lookup): reformat opcode table into 16x16 with shorter aliases
    Introduce addressing-mode aliases and an `op!` helper macro, and reformat
    LOOKUP_TABLE into a readable 16x16 (8+8) layout with aligned headers.

--- crates/nesium-core/src/cpu/lookup.rs
+++ crates/nesium-core/src/cpu/lookup.rs
@@ -1,277 +1,93 @@
 use crate::cpu::addressing::Addressing as A;
 use crate::cpu::instruction::Instruction as I;
 
+// Short aliases for addressing modes (to keep the 16x16 table readable)
+const IMP: A = A::Implied;
+const ACC: A = A::Accumulator;
+const IMM: A = A::Immediate;
+const REL: A = A::Relative;
+const ZP: A = A::ZeroPage;
+const ZPX: A = A::ZeroPageX;
+const ZPY: A = A::ZeroPageY;
+const ABS: A = A::Absolute;
+const ABX: A = A::AbsoluteX;
+const ABY: A = A::AbsoluteY;
+const IND: A = A::Indirect;
+const INX: A = A::IndirectX;
+const INY: A = A::IndirectY;
+
+macro_rules! op {
+    ($ins:ident, $addr:ident) => {
+        I::$ins($addr)
+    };
+}
+
+#[rustfmt::skip]
 pub(crate) static LOOKUP_TABLE: [I; 256] = [
-    //  0x00          0x01          0x02          0x03          0x04          0x05          0x06          0x07
-    I::brk(A::Implied),
-    I::ora(A::IndirectX),
-    I::jam(A::Implied),
-    I::slo(A::IndirectX),
-    I::nop(A::ZeroPage),
-    I::ora(A::ZeroPage),
-    I::asl(A::ZeroPage),
-    I::slo(A::ZeroPage),
-    I::php(A::Implied),
-    I::ora(A::Immediate),
-    I::asl(A::Accumulator),
-    I::anc(A::Immediate),
-    I::nop(A::Absolute),
-    I::ora(A::Absolute),
-    I::asl(A::Absolute),
-    I::slo(A::Absolute),
-    //  0x10          0x11          0x12          0x13          0x14          0x15          0x16          0x17
-    I::bpl(A::Relative),
-    I::ora(A::IndirectY),
-    I::jam(A::Implied),
-    I::slo(A::IndirectY),
-    I::nop(A::ZeroPageX),
-    I::ora(A::ZeroPageX),
-    I::asl(A::ZeroPageX),
-    I::slo(A::ZeroPageX),
-    I::clc(A::Implied),
-    I::ora(A::AbsoluteY),
-    I::nop(A::Implied),
-    I::slo(A::AbsoluteY),
-    I::nop(A::AbsoluteX),
-    I::ora(A::AbsoluteX),
-    I::asl(A::AbsoluteX),
-    I::slo(A::AbsoluteX),
-    //  0x18          0x19          0x1A          0x1B          0x1C          0x1D          0x1E          0x1F
-    I::jsr(A::Absolute),
-    I::and(A::IndirectX),
-    I::jam(A::Implied),
-    I::rla(A::IndirectX),
-    I::bit(A::ZeroPage),
-    I::and(A::ZeroPage),
-    I::rol(A::ZeroPage),
-    I::rla(A::ZeroPage),
-    I::plp(A::Implied),
-    I::and(A::Immediate),
-    I::rol(A::Accumulator),
-    I::anc(A::Immediate),
-    I::bit(A::Absolute),
-    I::and(A::Absolute),
-    I::rol(A::Absolute),
-    I::rla(A::Absolute),
-    //  0x20          0x21          0x22          0x23          0x24          0x25          0x26          0x27
-    I::bmi(A::Relative),
-    I::and(A::IndirectY),
-    I::jam(A::Implied),
-    I::rla(A::IndirectY),
-    I::nop(A::ZeroPageX),
-    I::and(A::ZeroPageX),
-    I::rol(A::ZeroPageX),
-    I::rla(A::ZeroPageX),
-    I::sec(A::Implied),
-    I::and(A::AbsoluteY),
-    I::nop(A::Implied),
-    I::rla(A::AbsoluteY),
-    I::nop(A::AbsoluteX),
-    I::and(A::AbsoluteX),
-    I::rol(A::AbsoluteX),
-    I::rla(A::AbsoluteX),
-    //  0x28          0x29          0x2A          0x2B          0x2C          0x2D          0x2E          0x2F
-    I::rti(A::Implied),
-    I::eor(A::IndirectX),
-    I::jam(A::Implied),
-    I::sre(A::IndirectX),
-    I::nop(A::ZeroPage),
-    I::eor(A::ZeroPage),
-    I::lsr(A::ZeroPage),
-    I::sre(A::ZeroPage),
-    I::pha(A::Implied),
-    I::eor(A::Immediate),
-    I::lsr(A::Accumulator),
-    I::asr(A::Immediate),
-    I::jmp(A::Absolute),
-    I::eor(A::Absolute),
-    I::lsr(A::Absolute),
-    I::sre(A::Absolute),
-    //  0x30          0x31          0x32          0x33          0x34          0x35          0x36          0x37
-    I::bvc(A::Relative),
-    I::eor(A::IndirectY),
-    I::jam(A::Implied),
-    I::sre(A::IndirectY),
-    I::nop(A::ZeroPageX),
-    I::eor(A::ZeroPageX),
-    I::lsr(A::ZeroPageX),
-    I::sre(A::ZeroPageX),
-    I::cli(A::Implied),
-    I::eor(A::AbsoluteY),
-    I::nop(A::Implied),
-    I::sre(A::AbsoluteY),
-    I::nop(A::AbsoluteX),
-    I::eor(A::AbsoluteX),
-    I::lsr(A::AbsoluteX),
-    I::sre(A::AbsoluteX),
-    //  0x38          0x39          0x3A          0x3B          0x3C          0x3D          0x3E          0x3F
-    I::rts(A::Implied),
-    I::adc(A::IndirectX),
-    I::jam(A::Implied),
-    I::rra(A::IndirectX),
-    I::nop(A::ZeroPage),
-    I::adc(A::ZeroPage),
-    I::ror(A::ZeroPage),
-    I::rra(A::ZeroPage),
-    I::pla(A::Implied),
-    I::adc(A::Immediate),
-    I::ror(A::Accumulator),
-    I::arr(A::Immediate),
-    I::jmp(A::Indirect),
-    I::adc(A::Absolute),
-    I::ror(A::Absolute),
-    I::rra(A::Absolute),
-    //  0x40          0x41          0x42          0x43          0x44          0x45          0x46          0x47
-    I::bvs(A::Relative),
-    I::adc(A::IndirectY),
-    I::jam(A::Implied),
-    I::rra(A::IndirectY),
-    I::nop(A::ZeroPageX),
-    I::adc(A::ZeroPageX),
-    I::ror(A::ZeroPageX),
-    I::rra(A::ZeroPageX),
-    I::sei(A::Implied),
-    I::adc(A::AbsoluteY),
-    I::nop(A::Implied),
-    I::rra(A::AbsoluteY),
-    I::nop(A::AbsoluteX),
-    I::adc(A::AbsoluteX),
-    I::ror(A::AbsoluteX),
-    I::rra(A::AbsoluteX),
-    //  0x48          0x49          0x4A          0x4B          0x4C          0x4D          0x4E          0x4F
-    I::nop(A::Immediate),
-    I::sta(A::IndirectX),
-    I::nop(A::Immediate),
-    I::sax(A::IndirectX),
-    I::sty(A::ZeroPage),
-    I::sta(A::ZeroPage),
-    I::stx(A::ZeroPage),
-    I::sax(A::ZeroPage),
-    I::dey(A::Implied),
-    I::nop(A::Immediate),
-    I::txa(A::Implied),
-    I::xaa(A::Immediate),
-    I::sty(A::Absolute),
-    I::sta(A::Absolute),
-    I::stx(A::Absolute),
-    I::sax(A::Absolute),
-    //  0x50          0x51          0x52          0x53          0x54          0x55          0x56          0x57
-    I::bcc(A::Relative),
-    I::sta(A::IndirectY),
-    I::jam(A::Implied),
-    I::sha(A::IndirectY),
-    I::sty(A::ZeroPageX),
-    I::sta(A::ZeroPageX),
-    I::stx(A::ZeroPageY),
-    I::sax(A::ZeroPageY),
-    I::tya(A::Implied),
-    I::sta(A::AbsoluteY),
-    I::txs(A::Implied),
-    I::shs(A::AbsoluteY),
-    I::shy(A::AbsoluteX),
-    I::sta(A::AbsoluteX),
-    I::shx(A::AbsoluteY),
-    I::sha(A::AbsoluteY),
-    //  0x58          0x59          0x5A          0x5B          0x5C          0x5D          0x5E          0x5F
-    I::ldy(A::Immediate),
-    I::lda(A::IndirectX),
-    I::ldx(A::Immediate),
-    I::lax(A::IndirectX),
-    I::ldy(A::ZeroPage),
-    I::lda(A::ZeroPage),
-    I::ldx(A::ZeroPage),
-    I::lax(A::ZeroPage),
-    I::tay(A::Implied),
-    I::lda(A::Immediate),
-    I::tax(A::Implied),
-    I::lax(A::Immediate),
-    I::ldy(A::Absolute),
-    I::lda(A::Absolute),
-    I::ldx(A::Absolute),
-    I::lax(A::Absolute),
-    //  0x60          0x61          0x62          0x63          0x64          0x65          0x66          0x67
-    I::bcs(A::Relative),
-    I::lda(A::IndirectY),
-    I::jam(A::Implied),
-    I::lax(A::IndirectY),
-    I::ldy(A::ZeroPageX),
-    I::lda(A::ZeroPageX),
-    I::ldx(A::ZeroPageY),
-    I::lax(A::ZeroPageY),
-    I::clv(A::Implied),
-    I::lda(A::AbsoluteY),
-    I::tsx(A::Implied),
-    I::las(A::AbsoluteY),
-    I::ldy(A::AbsoluteX),
-    I::lda(A::AbsoluteX),
-    I::ldx(A::AbsoluteY),
-    I::lax(A::AbsoluteY),
-    //  0x68          0x69          0x6A          0x6B          0x6C          0x6D          0x6E          0x6F
-    I::cpy(A::Immediate),
-    I::cmp(A::IndirectX),
-    I::nop(A::Immediate),
-    I::dcp(A::IndirectX),
-    I::cpy(A::ZeroPage),
-    I::cmp(A::ZeroPage),
-    I::dec(A::ZeroPage),
-    I::dcp(A::ZeroPage),
-    I::iny(A::Implied),
-    I::cmp(A::Immediate),
-    I::dex(A::Implied),
-    I::sbx(A::Immediate),
-    I::cpy(A::Absolute),
-    I::cmp(A::Absolute),
-    I::dec(A::Absolute),
-    I::dcp(A::Absolute),
-    //  0x70          0x71          0x72          0x73          0x74          0x75          0x76          0x77
-    I::bne(A::Relative),
-    I::cmp(A::IndirectY),
-    I::jam(A::Implied),
-    I::dcp(A::IndirectY),
-    I::nop(A::ZeroPageX),
-    I::cmp(A::ZeroPageX),
-    I::dec(A::ZeroPageX),
-    I::dcp(A::ZeroPageX),
-    I::cld(A::Implied),
-    I::cmp(A::AbsoluteY),
-    I::nop(A::Implied),
-    I::dcp(A::AbsoluteY),
-    I::nop(A::AbsoluteX),
-    I::cmp(A::AbsoluteX),
-    I::dec(A::AbsoluteX),
-    I::dcp(A::AbsoluteX),
-    //  0x78          0x79          0x7A          0x7B          0x7C          0x7D          0x7E          0x7F
-    I::cpx(A::Immediate),
-    I::sbc(A::IndirectX),
-    I::nop(A::Immediate),
-    I::isc(A::IndirectX),
-    I::cpx(A::ZeroPage),
-    I::sbc(A::ZeroPage),
-    I::inc(A::ZeroPage),
-    I::isc(A::ZeroPage),
-    I::inx(A::Implied),
-    I::sbc(A::Immediate),
-    I::nop(A::Implied),
-    I::sbc(A::Immediate),
-    I::cpx(A::Absolute),
-    I::sbc(A::Absolute),
-    I::inc(A::Absolute),
-    I::isc(A::Absolute),
-    //  0x78          0x79          0x7A          0x7B          0x7C          0x7D          0x7E          0x7F
-    I::beq(A::Relative),
-    I::sbc(A::IndirectY),
-    I::jam(A::Implied),
-    I::isc(A::IndirectY),
-    I::nop(A::ZeroPageX),
-    I::sbc(A::ZeroPageX),
-    I::inc(A::ZeroPageX),
-    I::isc(A::ZeroPageX),
-    I::sed(A::Implied),
-    I::sbc(A::AbsoluteY),
-    I::nop(A::Implied),
-    I::isc(A::AbsoluteY),
-    I::nop(A::AbsoluteX),
-    I::sbc(A::AbsoluteX),
-    I::inc(A::AbsoluteX),
-    I::isc(A::AbsoluteX),
+    // 0               1               2               3               4               5               6               7
+    // 8               9               A               B               C               D               E               F
+
+    // 0x00
+    op!(brk, IMP), op!(ora, INX), op!(jam, IMP), op!(slo, INX), op!(nop, ZP),  op!(ora, ZP),  op!(asl, ZP),  op!(slo, ZP),
+    op!(php, IMP), op!(ora, IMM), op!(asl, ACC), op!(anc, IMM), op!(nop, ABS), op!(ora, ABS), op!(asl, ABS), op!(slo, ABS),
+
+    // 0x10
+    op!(bpl, REL), op!(ora, INY), op!(jam, IMP), op!(slo, INY), op!(nop, ZPX), op!(ora, ZPX), op!(asl, ZPX), op!(slo, ZPX),
+    op!(clc, IMP), op!(ora, ABY), op!(nop, IMP), op!(slo, ABY), op!(nop, ABX), op!(ora, ABX), op!(asl, ABX), op!(slo, ABX),
+
+    // 0x20
+    op!(jsr, ABS), op!(and, INX), op!(jam, IMP), op!(rla, INX), op!(bit, ZP),  op!(and, ZP),  op!(rol, ZP),  op!(rla, ZP),
+    op!(plp, IMP), op!(and, IMM), op!(rol, ACC), op!(anc, IMM), op!(bit, ABS), op!(and, ABS), op!(rol, ABS), op!(rla, ABS),
+
+    // 0x30
+    op!(bmi, REL), op!(and, INY), op!(jam, IMP), op!(rla, INY), op!(nop, ZPX), op!(and, ZPX), op!(rol, ZPX), op!(rla, ZPX),
+    op!(sec, IMP), op!(and, ABY), op!(nop, IMP), op!(rla, ABY), op!(nop, ABX), op!(and, ABX), op!(rol, ABX), op!(rla, ABX),
+
+    // 0x40
+    op!(rti, IMP), op!(eor, INX), op!(jam, IMP), op!(sre, INX), op!(nop, ZP),  op!(eor, ZP),  op!(lsr, ZP),  op!(sre, ZP),
+    op!(pha, IMP), op!(eor, IMM), op!(lsr, ACC), op!(asr, IMM), op!(jmp, ABS), op!(eor, ABS), op!(lsr, ABS), op!(sre, ABS),
+
+    // 0x50
+    op!(bvc, REL), op!(eor, INY), op!(jam, IMP), op!(sre, INY), op!(nop, ZPX), op!(eor, ZPX), op!(lsr, ZPX), op!(sre, ZPX),
+    op!(cli, IMP), op!(eor, ABY), op!(nop, IMP), op!(sre, ABY), op!(nop, ABX), op!(eor, ABX), op!(lsr, ABX), op!(sre, ABX),
+
+    // 0x60
+    op!(rts, IMP), op!(adc, INX), op!(jam, IMP), op!(rra, INX), op!(nop, ZP),  op!(adc, ZP),  op!(ror, ZP),  op!(rra, ZP),
+    op!(pla, IMP), op!(adc, IMM), op!(ror, ACC), op!(arr, IMM), op!(jmp, IND), op!(adc, ABS), op!(ror, ABS), op!(rra, ABS),
+
+    // 0x70
+    op!(bvs, REL), op!(adc, INY), op!(jam, IMP), op!(rra, INY), op!(nop, ZPX), op!(adc, ZPX), op!(ror, ZPX), op!(rra, ZPX),
+    op!(sei, IMP), op!(adc, ABY), op!(nop, IMP), op!(rra, ABY), op!(nop, ABX), op!(adc, ABX), op!(ror, ABX), op!(rra, ABX),
+
+    // 0x80
+    op!(nop, IMM), op!(sta, INX), op!(nop, IMM), op!(sax, INX), op!(sty, ZP),  op!(sta, ZP),  op!(stx, ZP),  op!(sax, ZP),
+    op!(dey, IMP), op!(nop, IMM), op!(txa, IMP), op!(xaa, IMM), op!(sty, ABS), op!(sta, ABS), op!(stx, ABS), op!(sax, ABS),
+
+    // 0x90
+    op!(bcc, REL), op!(sta, INY), op!(jam, IMP), op!(sha, INY), op!(sty, ZPX), op!(sta, ZPX), op!(stx, ZPY), op!(sax, ZPY),
+    op!(tya, IMP), op!(sta, ABY), op!(txs, IMP), op!(shs, ABY), op!(shy, ABX), op!(sta, ABX), op!(shx, ABY), op!(sha, ABY),
+
+    // 0xA0
+    op!(ldy, IMM), op!(lda, INX), op!(ldx, IMM), op!(lax, INX), op!(ldy, ZP),  op!(lda, ZP),  op!(ldx, ZP),  op!(lax, ZP),
+    op!(tay, IMP), op!(lda, IMM), op!(tax, IMP), op!(lax, IMM), op!(ldy, ABS), op!(lda, ABS), op!(ldx, ABS), op!(lax, ABS),
+
+    // 0xB0
+    op!(bcs, REL), op!(lda, INY), op!(jam, IMP), op!(lax, INY), op!(ldy, ZPX), op!(lda, ZPX), op!(ldx, ZPY), op!(lax, ZPY),
+    op!(clv, IMP), op!(lda, ABY), op!(tsx, IMP), op!(las, ABY), op!(ldy, ABX), op!(lda, ABX), op!(ldx, ABY), op!(lax, ABY),
+
+    // 0xC0
+    op!(cpy, IMM), op!(cmp, INX), op!(nop, IMM), op!(dcp, INX), op!(cpy, ZP),  op!(cmp, ZP),  op!(dec, ZP),  op!(dcp, ZP),
+    op!(iny, IMP), op!(cmp, IMM), op!(dex, IMP), op!(sbx, IMM), op!(cpy, ABS), op!(cmp, ABS), op!(dec, ABS), op!(dcp, ABS),
+
+    // 0xD0
+    op!(bne, REL), op!(cmp, INY), op!(jam, IMP), op!(dcp, INY), op!(nop, ZPX), op!(cmp, ZPX), op!(dec, ZPX), op!(dcp, ZPX),
+    op!(cld, IMP), op!(cmp, ABY), op!(nop, IMP), op!(dcp, ABY), op!(nop, ABX), op!(cmp, ABX), op!(dec, ABX), op!(dcp, ABX),
+
+    // 0xE0
+    op!(cpx, IMM), op!(sbc, INX), op!(nop, IMM), op!(isc, INX), op!(cpx, ZP),  op!(sbc, ZP),  op!(inc, ZP),  op!(isc, ZP),
+    op!(inx, IMP), op!(sbc, IMM), op!(nop, IMP), op!(sbc, IMM), op!(cpx, ABS), op!(sbc, ABS), op!(inc, ABS), op!(isc, ABS),
+
+    // 0xF0
+    op!(beq, REL), op!(sbc, INY), op!(jam, IMP), op!(isc, INY), op!(nop, ZPX), op!(sbc, ZPX), op!(inc, ZPX), op!(isc, ZPX),
+    op!(sed, IMP), op!(sbc, ABY), op!(nop, IMP), op!(isc, ABY), op!(nop, ABX), op!(sbc, ABX), op!(inc, ABX), op!(isc, ABX),
 ];
