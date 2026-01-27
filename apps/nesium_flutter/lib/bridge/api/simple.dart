ec2d5a036ddeca7b96cc4fe3e02e6c6ab97e1a0c
mikai <dreamfever2017@yahoo.com>
2026-01-27 21:54:28 +0800

    chore: regenerate frb

--- apps/nesium_flutter/lib/bridge/api/simple.dart
+++ apps/nesium_flutter/lib/bridge/api/simple.dart
@@ -6,6 +6,8 @@
 import '../frb_generated.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
+// These functions are ignored because they are not marked as `pub`: `init_logging`
+
 String greet({required String name}) =>
     RustLib.instance.api.crateApiSimpleGreet(name: name);
 
